17a87bed71c22e67840c958c0db5386ba1e01212
12334-max <javi9876789@gmail.com>
2022-06-04 23:47:11 -0500

    escribi más texto (las noticias más recientes).

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -5,8 +5,8 @@
     <div class="hero">
         <div class="container">
             <div class="left">
-                <h1 class="h1">Bienvenido al<br><b>"Blog de deportes Machos Alfa"</b></h1>
-                <p class="h3">Encontraras un mundo deportivo</p>
+                <h1 class="h1">Bienvenido al<br><b>"Blog de deportes"</b></h1>
+                <p class="h3">Encontraras un mundo deportivo con las noticias más recientes</p>
                 <div class="btn-group">
                     <a href="#posts" class="btn btn-primary">Ver Posts</a>
                     <a href="{{ url("/suscribirse") }}" class="btn btn-secondary">Suscribirme</a>
