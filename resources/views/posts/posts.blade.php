94742f0495c92112821d077b3e283d30e3f15721
OmarRPL06 <omarperezrl38@gmail.com>
2022-06-06 00:19:12 -0500

    Desde mi nueva rama al main 3

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -46,45 +46,25 @@
                         <div class="icon-box">
                             <ion-icon name="football"></ion-icon>
                         </div>
-<<<<<<< HEAD
                         <p>Partidos de Futbol</p>
-=======
-                        <p>Liga de Campeones de la UEFA</p>
->>>>>>> 0bb1759a4078465ab2d18e373088408c44d4e520
                     </a>
                     <a href="#" class="topic-btn" id="posts-basquetbol">
                         <div class="icon-box">
                             <ion-icon name="basketball"></ion-icon>
                         </div>
-<<<<<<< HEAD
                         <p>Partidos de Basquetbol</p>
-=======
-                        <p>La Premier League</p>
->>>>>>> 0bb1759a4078465ab2d18e373088408c44d4e520
                     </a>
                     <a href="#" class="topic-btn" id="posts-beisbol">
                         <div class="icon-box">
                             <ion-icon name="baseball"></ion-icon>
                         </div>
-<<<<<<< HEAD
                         <p>Partidos de Béisbol</p>
-=======
-                        <p>La Liga Santander</p>
->>>>>>> 0bb1759a4078465ab2d18e373088408c44d4e520
                     </a>
                     <a href="#" class="topic-btn" id="posts-tenis">
                         <div class="icon-box">
                             <ion-icon name="tennisball"></ion-icon>
                         </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <p>Partidos de Tenis</p>
-=======
-                        <p>La Bundesliga</p>
->>>>>>> 0bb1759a4078465ab2d18e373088408c44d4e520
-=======
-                        <p>Partidos de Tenis</p>
->>>>>>> d19d2b15d54720214c1206874364d891ce51daf5
                     </a>
                 </div>
 
